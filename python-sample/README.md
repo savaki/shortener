3b092344282e05a7aa4b3a4458c8dc9d7d9e8b88
savaki <matt.ho@gmail.com>
2018-04-24 14:33:32 -0700

    added python example for comparison

---
+++ python-sample/README.md
@@ -0,0 +1,4 @@
+python-sample
+--------------
+
+provides a minimalistic python example that deploys to us-east-1 (see up.json)
---
+++ python-sample/app.py
@@ -0,0 +1,15 @@
+import os
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+
+
+class myHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        self.wfile.write("Hello World from Python\n")
+        return
+
+
+server = HTTPServer(('', int(os.environ['PORT'])), myHandler)
+server.serve_forever()
